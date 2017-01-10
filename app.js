219db0b811e8b53326e2f684959ca34dfdc0a4db
Masayuki Fuse <fuse@jp.ibm.com>
2017-01-10 05:56:00 +0000

    all

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
-/* jshint node: true */
-var http = require('http');
-var port = (process.env.VCAP_APP_PORT || 3000);
+/*eslint-env node*/
 
-var server = http.createServer(function (req, res) {
-	console.log("test");
-	res.writeHead(200, {
-		"Content-Type": "text/plain"
-	});
-	res.end("Hello, World!");
+//------------------------------------------------------------------------------
+// hello world app is based on node.js starter application for Bluemix
+//------------------------------------------------------------------------------
+
+// This application uses express as its web server
+// for more info, see: http://expressjs.com
+var express = require('express');
+
+// cfenv provides access to your Cloud Foundry environment
+// for more info, see: https://www.npmjs.com/package/cfenv
+var cfenv = require('cfenv');
+
+// create a new express server
+var app = express();
+
+// serve the files out of ./public as our main files
+app.use(express.static(__dirname + '/public'));
+
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
+// start server on the specified port and binding host
+app.listen(appEnv.port, '0.0.0.0', function() {
+
+	// print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
 });
-
-server.listen(port);
-console.log("Server listening on port " + port);
